618f3573aa0f75e9470d8f54b47a0721079dd5a8
nazar <cutlass900@gmail.com>
2023-07-11 22:13:07 -0400

    works

--- main.py
+++ main.py
@@ -11,10 +11,9 @@ pipe.to(device)
 pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
 image = Image.open('data/nazar person.png')
 latents = torch.randn([1, 4, 64, 64]).to(device).half()
-generator = torch.Generator(device=device).manual_seed(42)
-denoised_latent = pipe(prompt, image=image, num_inference_steps=15, latents=latents, generator=generator, output_type="latent")
-# denoised_latent = pipe(prompt, image=image, num_inference_steps=15, latents=latents, generator=generator)
-result_tensor = pipe.vae.decode(denoised_latent.images / pipe.vae.config.scaling_factor, return_dict=False)[0]
-result_pil = pipe.image_processor.postprocess(result_tensor, output_type="pil", do_denormalize=[True])
-
+generator = torch.Generator(device=device).manual_seed(43)
+denoised_latent = pipe(prompt, image=image, num_inference_steps=15, latents=latents, generator=generator, output_type="latent", guidance_scale=10)
+with torch.no_grad():
+    result_tensor = pipe.vae.decode(denoised_latent.images / pipe.vae.config.scaling_factor, return_dict=False)[0].detach()
+    result_pil = pipe.image_processor.postprocess(result_tensor, output_type="pil", do_denormalize=[True])[0]
 result_pil.save('data/'+prompt+'.png')
