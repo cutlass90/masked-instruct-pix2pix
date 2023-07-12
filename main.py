4dad68adcf4599d9b797cc989d9d1b76ef534098
nazar <cutlass900@gmail.com>
2023-07-11 22:21:56 -0400

    masks works well

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from PIL import Image
 
 prompt = "add a knight helmet on his head"
 device = 'cuda:0'
+mask = torch.zeros([1, 1, 64, 64]).to(device).half()
+mask[:,:,:, :32] = 1
 
 model_id = "timbrooks/instruct-pix2pix"
 pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained(model_id, torch_dtype=torch.float16, safety_checker=None)
@@ -12,8 +14,22 @@ pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.conf
 image = Image.open('data/nazar person.png')
 latents = torch.randn([1, 4, 64, 64]).to(device).half()
 generator = torch.Generator(device=device).manual_seed(43)
-denoised_latent = pipe(prompt, image=image, num_inference_steps=15, latents=latents, generator=generator, output_type="latent", guidance_scale=10)
+
 with torch.no_grad():
-    result_tensor = pipe.vae.decode(denoised_latent.images / pipe.vae.config.scaling_factor, return_dict=False)[0].detach()
+    encoded_latent = pipe.prepare_image_latents(
+                pipe.image_processor.preprocess(image),
+                1,
+                1,
+                torch.float16,
+                device,
+                False,
+                generator,
+            )
+
+
+
+denoised_latent = pipe(prompt, image=image, num_inference_steps=15, latents=latents, generator=generator, output_type="latent", guidance_scale=10).images / pipe.vae.config.scaling_factor
+with torch.no_grad():
+    result_tensor = pipe.vae.decode(denoised_latent*mask+encoded_latent*(1-mask), return_dict=False)[0].detach()
     result_pil = pipe.image_processor.postprocess(result_tensor, output_type="pil", do_denormalize=[True])[0]
 result_pil.save('data/'+prompt+'.png')
